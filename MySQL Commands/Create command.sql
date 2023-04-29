63162f703e163c41741eeebc2edc79ab0fd19f38
Suraj Usrethe <104275415+surajusrethe@users.noreply.github.com>
2023-04-30 00:52:29 +0530

    Create command.sql
    Create command.sql

---
+++ MySQL Commands/Create command.sql
@@ -0,0 +1,16 @@
+create database amitdb;
+
+ show database;
+ 
++--------------------+
+| Database           |
++--------------------+
+| amitdb             |
+| information_schema |
+| mysql              |
+| performance_schema |
+| surajdb            |
+| surajdb1           |
+| sys                |
+| trial2             |
++--------------------+
