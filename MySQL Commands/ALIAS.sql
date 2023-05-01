ae0149f2fd85dfb1c17c6c8c65a8510c80297d82
Suraj Usrethe <104275415+surajusrethe@users.noreply.github.com>
2023-05-01 20:19:17 +0530

    ALIAS.sql
    ALIAS.sql

--- MySQL Commands/ALIAS.sql
+++ MySQL Commands/ALIAS.sql
@@ -1,1 +1,43 @@
+-- ALIAS: Temporary name given to a table or column while showing results.
+--        ALIAS makes the column more readable.
+--        It only exist for the duration of the query.
 
+
+
+...................................................................................................................................................
+
+-- EXAMPLE: suppose we want the AGE from the table but we do not want the name to be age becasue it is not descript engough to catch in one go.
+
+
+mysql> SELECT * FROM EMPLOYEES;
++------+------------+------+--------+
+| ID   | NAME       | AGE  | SALARY |
++------+------------+------+--------+
+|   11 | MONU PATEL |   35 |  50000 |
+|    2 | Sarah      |   25 |  45000 |
+|    3 | David      |   40 |  75000 |
+|    4 | Mary       |   35 |  65000 |
++------+------------+------+--------+
+
+mysql> SELECT AGE AS age_of_employee
+    -> FROM employees;
++-----------------+
+| age_of_employee |
++-----------------+
+|              35 |
+|              25 |
+|              40 |
+|              35 |
++-----------------+
+
+
+ SELECT NAME AS naam, AGE AS umar, SALARY AS tankhuva
+    -> FROM EMPLOYEEs;
++------------+------+----------+
+| naam       | umar | tankhuva |
++------------+------+----------+
+| MONU PATEL |   35 |    50000 |
+| Sarah      |   25 |    45000 |
+| David      |   40 |    75000 |
+| Mary       |   35 |    65000 |
++------------+------+----------+
