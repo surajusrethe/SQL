4b31288e788fcb776121fce3e529b3fd6f092933
Suraj Usrethe <104275415+surajusrethe@users.noreply.github.com>
2023-05-03 01:33:11 +0530

    JOINS.sql
    JOINS.sql

---
+++ MySQL Commands/JOINS.sql
@@ -0,0 +1,20 @@
+-- JOINS: Joins are an essential feature in SQL that enable us to combine data from two or more tables based on a common column.
+
+
+
+-- Inner Join:  returns only the matching rows from both tables based on the join condition.
+
+
+-- Left Join: returns all the rows from the left table and the matching rows from the right table based on the join condition.
+   If there is no match in the right table, NULL values are returned.
+
+
+
+-- Right Join: returns all the rows from the right table and the matching rows from the left table based on the join condition.
+   If there is no match in the left table, NULL values are returned.
+
+
+
+-- Outer Join:
+
+
