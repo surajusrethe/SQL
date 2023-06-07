20b31fc7e5b1730cac0886c2be50af5d361b10eb
Suraj Usrethe <104275415+surajusrethe@users.noreply.github.com>
2023-06-07 23:44:49 +0530

    Update Normalization.sql

--- MySQL Commands/Normalization.sql
+++ MySQL Commands/Normalization.sql
@@ -1,16 +1,12 @@
  Normalization: Normalization is the process of organizing a database in such a way that it reduces redundancy and dependency among the data.
  
  Deormalization: Denormalization is the process of intentionally adding redundancy to a database table in order to improve performance or simplify data retrieval.
+ 
 
-
-
-
-
-
-
-
-
-The main difference between normalization and denormalization is that normalization is the process of reducing redundancy and dependency in a database by breaking down a table into smaller, more manageable tables and establishing relationships between them, while denormalization is the process of intentionally adding redundancy to a database table in order to improve performance or simplify data retrieval. Normalization is used to ensure data integrity and consistency, while denormalization is used to improve query performance.
+The main difference between normalization and denormalization is that normalization is the process of reducing redundancy and dependency in a database by
+breaking down a table into smaller, more manageable tables and establishing relationships between them, while denormalization is the process of intentionally 
+adding redundancy to a database table in order to improve performance or simplify data retrieval. Normalization is used to ensure data integrity and consistency
+, while denormalization is used to improve query performance.
 
 
 
